a9b5ea126ce3dcfe6f309939722930f20fef7626
mmajd <mabulmagd55@gmail.com>
2023-10-14 21:28:21 +0300

    add: adapters init

--- src/main/java/com/invola/system/adapter/out/persistence/UserEntity.java
+++ src/main/java/com/invola/system/adapter/out/persistence/UserEntity.java
@@ -2,6 +2,7 @@ package com.invola.system.adapter.out.persistence;
 
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserEntity {
     @Id
+    @Column(name = "user_email")
     private String email;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
